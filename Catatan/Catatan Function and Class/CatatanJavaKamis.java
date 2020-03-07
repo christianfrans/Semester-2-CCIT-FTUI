fde72c3f549c8d4e93f8b28722276de810d11728
christianfrans <61776387+christianfrans@users.noreply.github.com>
2020-03-07 16:59:48 +0700

    Add files via upload

---
+++ Catatan/Catatan Function and Class/CatatanJavaKamis.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catatan.java.kamis;
+
+/**
+ *
+ * @author user
+ */
+public class CatatanJavaKamis {
+
+    /**
+     * @param args the command line arguments
+     * @param cars
+     */
+    public static void main(String[] args, String[] cars) {
+        // TODO code application logic here
+        Hello hello = new Hello();
+        String words = hello.sayHello();
+        
+        System.out.println(words);
+        
+        hello.sayHelloAgain();
+        
+        System.out.println("");
+    }
+}
---
+++ Catatan/Catatan Function and Class/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catatan.java.kamis;
+
+/**
+ *
+ * @author user
+ */
+public class Hello {
+    
+    public void sayHelloAgain(){
+        System.out.println("Hello World");
+    }
+    public String sayHello(){
+        return "Hello World";
+    }
+}
